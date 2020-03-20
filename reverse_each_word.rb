8bd8a2ccf6558dec76633ad068ba82570a49d207
Robert Phillips <rpdecks@gmail.com>
2020-03-20 15:20:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def reverse_each_word(string)
   input = string.split(/ /)
-  input.each do 
-    input = input.reverse
+  input.each do |memo|
+    "#{memo}.reverse
   end
   p input.join(" ")
   #binding.pry
