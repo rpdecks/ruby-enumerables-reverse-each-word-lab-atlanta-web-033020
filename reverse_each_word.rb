ac335f5ed58edd61d5a7cce1b32e7d56de5bcdfc
Robert Phillips <rpdecks@gmail.com>
2020-03-20 15:44:51 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+def reverse_each_word(string)
+  input = string.split(" ")
+  reversed_array = input.collect do |memo|
+    memo.reverse
+  end
+  reversed_array.join(" ")
+  #binding.pry
+end
+
+
