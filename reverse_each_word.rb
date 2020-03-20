6b16a8d81cfbf613fdf11d0e5820eecdaac69486
Robert Phillips <rpdecks@gmail.com>
2020-03-20 15:20:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,10 +11,3 @@ end
 
 
 
-def eachExample(string)
-  input = string.split(" ")
-  input.each do |memo| 
-    #here with in the each enumerable memo refers to each element of the input array, the first time through it refers to input[0] the second time input[1] the third input[2] and so on.... so for this reason we do not need to be able to access the index, each does that for us
-    memo.upcase
-  end
-end
