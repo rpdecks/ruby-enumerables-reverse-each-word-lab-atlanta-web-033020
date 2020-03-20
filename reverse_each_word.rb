9da7f026d98c36ceb4493840601dd474c3ec98e7
Robert Phillips <rpdecks@gmail.com>
2020-03-20 15:36:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(string)
   input = string.split(" ")
   reversed_array = []
-  input.each do |memo|
+  input.collect do |memo|
     reversed_array.push(memo.reverse)
   end
   reversed_array.join(" ")
